b614100530c061e535229fec504646733b795eab
Hendrik <hendrik.bloess@googlemail.com>
2018-05-19 19:59:01 +0200

    so, jetzt aber wirklich

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,29 @@
 <template>
   <div id="app">
-    <md-toolbar class="md-accent" md-elevation="1">
-      <h3 class="md-title" style="flex: 1">Title</h3>
-      <md-button>Refresh</md-button>
-      <md-button class="md-primary">Create</md-button>
-    </md-toolbar>
-    <img src="./assets/logo.png">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
     <router-view/>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'App'
-}
-</script>
-
-<style>
+<style lang="scss">
 #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>

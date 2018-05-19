f3e125e1745ee4f54e37be08e9facd08141a4b57
Hendrik <hendrik.bloess@googlemail.com>
2018-05-19 19:30:38 +0200

    jetzte mit ts

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -83,16 +83,7 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
-</script>
+<script src="./HelloWorldScript.ts"></script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
